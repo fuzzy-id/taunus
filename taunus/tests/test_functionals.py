2c78e3ec44eb85e75b2d747a8a6bc7b6da0104c7
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 00:52:45 +0100

    testing that app is initialized with home dir

--- taunus/tests/test_functionals.py
+++ taunus/tests/test_functionals.py
@@ -2,20 +2,26 @@ import unittest
 import tempfile
 import shutil
 from webtest import TestApp
+import taunus
+import pprint
+import os
 
 class ApplicationStartupTests(unittest.TestCase):
     
     def test_unexistent_directory_raises_error_test(self):
         with self.assertRaises(IOError):
-            import taunus
-            app = taunus.main({}, root='/non/existing/path/')
+            app = taunus.main({}, root_dir='/non/existing/path/')
+
+    def test_app_is_initialized_via_home(self):
+        app = TestApp(taunus.main({}))
+        req = app.get('/')
+        self.assertIn(os.environ['HOME'], req.body)
 
 class FunctionalTests(unittest.TestCase):
     
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
-        import taunus
-        app = taunus.main({'root': self.test_dir, })
+        app = taunus.main({}, root_dir=self.test_dir)
         self.app = TestApp(app)
 
     def tearDown(self):
