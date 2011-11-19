866479681df9ed7a2695600850b8e162285873af
Thomas Bach <bachth@uni-mainz.de>
2011-11-19 21:50:35 +0100

    first adaptions

--- taunus/__init__.py
+++ taunus/__init__.py
@@ -1,12 +1,12 @@
 from pyramid.config import Configurator
-from taunus.resources import Root
+from taunus.resources import RootDir
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('taunus.views.my_view',
-                    context='taunus:resources.Root',
-                    renderer='taunus:templates/mytemplate.pt')
-    config.add_static_view('static', 'taunus:static', cache_max_age=3600)
+    config = Configurator(root_factory=RootDir, 
+                          settings=settings)
+    config.add_static_view('static', 'taunus:static', 
+                           cache_max_age=3600)
+    config.scan(taunus.views)
     return config.make_wsgi_app()
