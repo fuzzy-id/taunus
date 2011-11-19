4f412b416c78214f381b611eea3f809c2be94855
Thomas Bach <bachth@uni-mainz.de>
2011-11-19 22:15:26 +0100

    added basic functional tests

---
+++ taunus/tests/test_functionals.py
@@ -0,0 +1,25 @@
+import unittest
+import tempfile
+import shutil
+from webtest import TestApp
+
+class ApplicationStartupTests(unittest.TestCase):
+    
+    def unexistent_directory_raises_error_test(self):
+        with self.assertRaises(IOError):
+            import taunus
+            app = taunus.main({}, root='/non/existing/path/')
+
+class FunctionalTests(unittest.TestCase):
+    
+    def setUp(self):
+        test_dir = tempfile.mkdtemp()
+        import taunus
+        app = taunus.main({'root': test_dir, })
+        self.app = TestApp(app)
+
+    def tearDown(self):
+        shutil.rmtree(test_dir)
+
+    def test_view_root(self):
+        pass
