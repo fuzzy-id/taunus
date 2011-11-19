4fcff99e2b13184cf70e703268345d67694b36d2
Thomas Bach <bachth@uni-mainz.de>
2011-11-19 22:07:49 +0100

    doing functional tests with webtest

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.org')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid', 'pyramid_debugtoolbar']
+requires = ['pyramid', 'pyramid_debugtoolbar', 'WebTest']
 
 setup(name='taunus',
       version='0.0',
