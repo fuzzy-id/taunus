525ccc180ea503937fbd477970b667169f70291b
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 00:28:23 +0100

    setting default_root in root factory

--- taunus/__init__.py
+++ taunus/__init__.py
@@ -1,10 +1,12 @@
 from pyramid.config import Configurator
-from taunus.resources import root_dir_factory
+from taunus.resources import RootDirFactory
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(root_factory=root_dir_factory, 
+    if 'root_dir' in settings:
+        RootDirFactory.set_default_root(settings['root_dir'])
+    config = Configurator(root_factory=RootDirFactory, 
                           settings=settings)
     config.add_static_view('static', 'taunus:static', 
                            cache_max_age=3600)
