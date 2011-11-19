4046a7f436b0f80fced734c24c42821d5ad3732b
Thomas Bach <bachth@uni-mainz.de>
2011-11-19 22:14:56 +0100

    creating a root_dir_factory

--- taunus/__init__.py
+++ taunus/__init__.py
@@ -1,12 +1,13 @@
 from pyramid.config import Configurator
-from taunus.resources import RootDir
+from taunus.resources import root_dir_factory
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(root_factory=RootDir, 
+    config = Configurator(root_factory=root_dir_factory, 
                           settings=settings)
     config.add_static_view('static', 'taunus:static', 
                            cache_max_age=3600)
+    import taunus.views
     config.scan(taunus.views)
     return config.make_wsgi_app()
