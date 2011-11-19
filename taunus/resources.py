40c8bc2a0a7fc1dc180aaf290ccc4dd0ef2ec1f7
Thomas Bach <bachth@uni-mainz.de>
2011-11-19 22:15:11 +0100

    added basic root_dir_factory

--- taunus/resources.py
+++ taunus/resources.py
@@ -1,3 +1,8 @@
-class Root(object):
-    def __init__(self, request):
+default_root = None
+
+def root_dir_factory(request):
+    pass
+
+class RootDir(object):
+    def __init__(self, dir, request):
         self.request = request
