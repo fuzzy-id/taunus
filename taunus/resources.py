3d1e9ad05575ed3d332cea62c40038fa1260f020
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 00:28:53 +0100

    basic root dir factory and root dir added

--- taunus/resources.py
+++ taunus/resources.py
@@ -1,8 +1,26 @@
-default_root = None
+# -*- coding: utf-8 -*-
+import os
 
-def root_dir_factory(request):
-    pass
+class RootDirFactory(object):
+    
+    _default_root = None
+
+    def __new__(cls, request):
+        
+        if cls._default_root is None:
+            return RootDir(os.environ['HOME'])
+        return RootDir(cls._default_root)
+
+    @classmethod
+    def set_default_root(cls, root_dir):
+        if not os.path.isdir(root_dir):
+            raise IOError("Couldn't find root dir '%s'." % root_dir)
+
 
 class RootDir(object):
-    def __init__(self, dir, request):
-        self.request = request
+
+    __name__ = None
+    __parent__ = None
+
+    def __init__(self, dir):
+        self.__name__ = dir
