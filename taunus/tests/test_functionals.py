34fda592c0a04c1050f2d544d335fe605130bfef
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 01:25:44 +0100

    renamed root_dir to default_root

--- taunus/tests/test_functionals.py
+++ taunus/tests/test_functionals.py
@@ -10,22 +10,23 @@ class ApplicationStartupTests(unittest.TestCase):
     
     def test_unexistent_directory_raises_error_test(self):
         with self.assertRaises(IOError):
-            app = taunus.main({}, root_dir='/non/existing/path/')
+            app = taunus.main({}, default_root='/non/existing/path/')
 
     def test_app_is_initialized_via_home(self):
         app = TestApp(taunus.main({}))
-        req = app.get('/')
-        self.assertIn(os.environ['HOME'], req.body)
+        resp = app.get('/')
+        self.assertIn(os.environ['HOME'], resp.body)
 
 class FunctionalTests(unittest.TestCase):
     
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
-        app = taunus.main({}, root_dir=self.test_dir)
+        app = taunus.main({}, default_root=self.test_dir)
         self.app = TestApp(app)
 
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
-    def test_view_root(self):
-        pass
+    def test_setting_default_dir_works(self):
+        resp = self.app.get('/')
+        self.assertIn(self.test_dir, resp.body)
