904db473bb923a8f4f0ad290417862b150508278
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 01:20:42 +0100

    renamed rootdir to directory

--- taunus/resources.py
+++ taunus/resources.py
@@ -8,8 +8,8 @@ class RootDirFactory(object):
     def __new__(cls, request):
         
         if cls._default_root is None:
-            return RootDir(os.environ['HOME'])
-        return RootDir(cls._default_root)
+            return Directory(os.environ['HOME'])
+        return Directory(cls._default_root)
 
     @classmethod
     def set_default_root(cls, root_dir):
@@ -17,10 +17,13 @@ class RootDirFactory(object):
             raise IOError("Couldn't find root dir '%s'." % root_dir)
 
 
-class RootDir(object):
+class Directory(object):
 
     __name__ = None
     __parent__ = None
 
     def __init__(self, dir):
         self.__name__ = dir
+
+    def __str__(self):
+        return self.__name__
