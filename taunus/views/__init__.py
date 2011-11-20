9ce8f5d9a633c21b1e9d6f8d104d3ca96fc4de88
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 01:21:40 +0100

    renamed root dir to directory

--- taunus/views/__init__.py
+++ taunus/views/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from pyramid.view import view_config
 
-@view_config(context='taunus.resources.RootDir',
+@view_config(context='taunus.resources.Directory',
              renderer='taunus:templates/dir_list.pt')
 def my_view(context, request):
     return {'directory': context}
