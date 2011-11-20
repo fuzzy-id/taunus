fe93b820436e93b0248fe821e93187bd12db6006
Thomas Bach <bachth@uni-mainz.de>
2011-11-20 01:19:04 +0100

    basic view to directory

--- taunus/views/__init__.py
+++ taunus/views/__init__.py
@@ -1,2 +1,7 @@
-def my_view(request):
-    return {'project':'taunus'}
+# -*- coding: utf-8 -*-
+from pyramid.view import view_config
+
+@view_config(context='taunus.resources.RootDir',
+             renderer='taunus:templates/dir_list.pt')
+def my_view(context, request):
+    return {'directory': context}
