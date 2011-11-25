d334b114c131330cfb9e202936b25a7db4139254
Thomas Bach <bachth@uni-mainz.de>
2011-11-25 23:21:26 +0100

    added base template subscriber

--- taunus/__init__.py
+++ taunus/__init__.py
@@ -9,6 +9,7 @@ def main(global_config, **settings):
                           settings=settings)
     config.add_static_view('static', 'taunus:static', 
                            cache_max_age=3600)
-    import taunus.views
-    config.scan(taunus.views)
+    config.add_subscriber('taunus.subscribers.add_base_template',
+                          'pyramid.events.BeforRender')
+    config.scan('taunus.views')
     return config.make_wsgi_app()
