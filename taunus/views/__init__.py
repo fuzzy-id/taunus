dd3e300599db0c2d4ee2694af4c48db2ed3fc18e
Thomas Bach <bachth@uni-mainz.de>
2011-11-29 13:16:21 +0100

    removed unnecesary listingentry class

--- taunus/views/__init__.py
+++ taunus/views/__init__.py
@@ -4,25 +4,10 @@ from pyramid.response import Response
 from pyramid.traversal import resource_path
 from pyramid.view import view_config
 
-class ListingEntry(object):
-    
-    def __init__(self, entry, request):
-        self.entry = entry
-        self.request = request
-
-    def __str__(self):
-        return str(self.entry)
-
-    @property
-    def path(self):
-        return self.entry.path
-
 @view_config(context='taunus.resources.Directory',
              renderer='taunus:templates/directory.pt')
 def view_directory(context, request):
     return {'resource': context,
-            'listing': [ ListingEntry(entry, request)
-                         for entry in context ], 
             'root_to_resource': [ (r, resource_path(r), )
                                   for r in reversed(
                 [ c for c in lineage(context) ]) ], }
